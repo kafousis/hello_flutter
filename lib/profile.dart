494d7f47a4321e6d1c70146b22ae3cd921076dd4
Giannis Kafousis <g.kafousis@gmail.com>
2022-09-13 17:16:13 +0300

    ListView

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
+const int itemCount = 20;
+
 class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+        itemCount: itemCount,
+        itemBuilder: (BuildContext context, int index) {
+      return ListTile(
+        title: Text('Item ${(index+1)}'),
+        leading: const Icon(Icons.person),
+        trailing: const Icon(Icons.select_all),
+        onTap: (){
+          debugPrint('Item ${(index+1)} selected');
+        },
+      );
+    });
   }
 }
